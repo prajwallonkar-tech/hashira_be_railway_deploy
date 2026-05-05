f52bb0735f170f92c134ccd04ea66e97bd122843
Prajwal Lonkar <prajwal.lonkar@techalchemy.com>
2026-05-05 14:02:44 +0530

    removed folders

--- scripts/export-swagger.ts
+++
@@ -1,8 +0,0 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import * as yaml from 'js-yaml';
-import { swaggerSpec } from '../src/config/swagger';
-
-const outputPath = path.resolve(__dirname, '../swagger.yaml');
-fs.writeFileSync(outputPath, yaml.dump(swaggerSpec, { noRefs: true }));
-console.log(`Swagger spec exported to ${outputPath}`);
